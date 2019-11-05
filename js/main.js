9c39364a1b88dd6badf71e2fba52b3152ffd1d2c
Petrova <e_petrova@ats-hr420-dm16.fc.ca>
2019-11-05 14:39:00 -0500

    ahaha

---
+++ js/main.js
@@ -0,0 +1,30 @@
+(() => {
+	console.log('fired!');
+
+	let player_lives = 5,
+	    player = document.querySelector("#weapon"),
+	    computer,
+	    trigger = document.querySelector("#play"),
+	    status =document.querySelector(".winlose"),
+	    computer_lives = 5;
+
+	const choices = ["rock", "paper", "scissors"];
+
+	function playGame() {
+		console.log('clicked the button');
+
+		computer = choices[Math.floor(Math.random() * choices.length)];
+		let player_choice = player.value;
+		console.log("computer: ", computer, "player: ", player_choice);
+		// this is a comment. check for a tie
+		if(computer == player_choice){
+			//tie - no wins
+			status.textContent = "Tie! You live to shoot another day!";
+		}
+
+	}
+
+	trigger.addEventListener("click", playGame);
+
+
+})();
